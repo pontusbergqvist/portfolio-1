4298c6fea71d1a8786ed5a0310914b2e1346264a
Pontus Bergqvist <pontus@Pontuss-MBP.lan>
2022-03-28 01:38:53 +0200

    TailwindCSS config and React Icons

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#343434',
+      },
+      fontSize: {
+        heading: '80px',
+        headingSecondary: '40px',
+        headingTertiary: '30px',
+        body: '20px',
+      },
+      fontFamily: {
+        dankMono: ['Dank Mono', 'sans-serif'],
+        plexSerif: ['IBM Plex Serif', 'sans-serif'],
+        verdana: ['Verdana', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
