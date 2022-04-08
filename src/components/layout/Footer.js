530c4739dcf012febb211882305197b09edfe008
Pontus <pontus@Pontuss-MBP.lan>
2022-04-08 22:17:01 +0200

    Smol footer adjustment

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import { FaTwitter, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className='max-w-[1400px] h-[420px] px-3 mx-auto flex flex-col justify-center font-dankMono text-primary  dark:text-darkText before:content-[""] before:h-[2px] before:w-[80%] before:max-w-[350px] before:block before:bg-primary before:mb-5 before:dark:bg-darkText'>
+    <footer className='max-w-[1400px] h-[430px] px-3 mx-auto flex flex-col justify-center font-dankMono text-primary  dark:text-darkText before:content-[""] before:h-[2px] before:w-[80%] before:max-w-[350px] before:block before:bg-primary before:mb-5 before:dark:bg-darkText'>
       <p className='font-verdana my-5 sm:m-0'>Get in touch..</p>
       <p className='text-body dontUseGalaxyFold:text-headingTertiary md:text-headingSecondary my-2'>email: pontus@bergqvist.io</p>
       <p className='text-body dontUseGalaxyFold:text-headingTertiary md:text-headingSecondary my-2'>github: pontusbergqvist</p>
